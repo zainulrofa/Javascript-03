cc338aee39b9e8b91595deaf4dd7cef36ab33277
Zainul Muhammad Rofa <113880540+zainulrofa@users.noreply.github.com>
2022-09-30 08:35:58 +0700

    Update cekHariKerja.js

--- cekHariKerja.js
+++ cekHariKerja.js
@@ -14,13 +14,16 @@ const cekHariKerja = (day) => {
         }, 3000)
     })
 }
+// di tahap ini berada pada keadaan pending dimana nilai masih belum diketahui antara resolve atau reject
 
 cekHariKerja('rabu').then((solved) => {
         console.log(`${solved} adalah hari kerja`)
     })
+ //.then tempat kita memasukkan cb function untuk nilai resolve  
     .catch((err) => {
         console.log(err.message)
     })
+//.catch tempat kita memasukkan cb function untuk nilai rejected 
 
 // try catch version
 async function hariKerja(cek, hariKerjaFunc) {
@@ -28,8 +31,10 @@ async function hariKerja(cek, hariKerjaFunc) {
         if (typeof hariKerjaFunc !== "function") throw new Error('Invalid Parameter')
         const response = await hariKerjaFunc(cek)
         return console.log(`${response} adalah hari kerja`)
+ // try digunakan untuk menyimpan code yang masih memungkinkan terjadinya error
     } catch (err) {
         return console.log(err.message)
+// catch digunakan untuk menangkap error yang muncul di try
     }
 }
 
